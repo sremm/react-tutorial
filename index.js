6ac9b33470f14f8918046e2809846125b8f99ae1
Sten Remmelg <sten.remmelg@gmail.com>
2021-03-31 21:54:05 +0200

    props and styling example

---
+++ index.js
@@ -0,0 +1,11 @@
+ReactDOM.render(
+    React.createElement(
+        'a',
+        {
+            id: 'home',
+            href: 'https://frontarm.com/',
+        },
+        'Frontend Armory',
+    ),
+    document.getElementById('root')
+)
