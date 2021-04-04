99423156c4d9e092bbd682ce165fbe30aa35606b
Sten Remmelg <sten.remmelg@gmail.com>
2021-04-04 22:59:02 +0200

    create a contacts list from list of billionares

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './styles.css'
+
+let billionaires = [
+  { name: 'Bill Gates', email: 'billg@microsoft.com' },
+  { name: 'Jeff Bezos', email: 'jeff@amazon.com' },
+  { name: 'Mark Zuckerberg', email: 'zuck@fb.com' },
+]
+
+function getInitials(name) {
+  var firstNameInitial = name.split(" ")[0][0]
+  var lastNameInitial = name.split(" ")[1][0]
+  var result = firstNameInitial + lastNameInitial
+  return result
+}
+var billionairesList = []
+for (const billionare of billionaires) {
+  billionairesList.push(
+    <div className='Contact'>
+      <div className='Contact-avatar'>{getInitials(billionare.name)}</div>
+      <span className='Contact-name'>{billionare.name}</span>
+      <a href={'mailto:' + billionare.email}>
+        {billionare.email}
+      </a>
+    </div>
+  )
+
+}
+
+// Your React elements will go here.
+let elements = billionairesList
+
+// Use a for loop or array.map to build the elements array
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div className='ContactList'>{elements}</div>,
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
