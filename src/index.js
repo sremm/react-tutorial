ecdd806f7521a63e3849ad12984ac5f89407ad33
Sten Remmelg <sten.remmelg@gmail.com>
2021-04-07 22:27:28 +0200

    add example button to add contact to list

--- src/index.js
+++ src/index.js
@@ -9,8 +9,30 @@ function ContactList(props) {
   return <div className='ContactList'>{props.children}</div>
 }
 
-let contactElements = billionaires.map(function (contact, index) { return <Contact contact={contact} key={index}></Contact> })
+function App(props) {
+  return <ContactList>
+    {props.contacts.map((contact, i) =>
+      <Contact contact={contact} key={i}></Contact>
+    )}
+    <button onClick={update}>
+      Add
+    </button>
+  </ContactList>
+}
+
+function update() {
+  let updatedContacts = billionaires.concat({
+    name: 'Masayoshi Son',
+    email: 'mson@softbank.co.jp'
+  })
+
+  ReactDOM.render(
+    <App contacts={updatedContacts}></App>,
+    document.getElementById('root')
+  )
+}
+
 ReactDOM.render(
-  <ContactList >{contactElements}</ContactList>,
+  <App contacts={billionaires}></App>,
   document.getElementById('root')
 )
